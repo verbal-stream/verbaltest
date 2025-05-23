cec88268f0c29fc861aec1b4f741135d11987b02
DonsWayo <juan.carracedo@asafedigital.com>
2025-05-23 09:34:56 +0200

    feat(api): export API decorators from package index

--- packages/playwright-decorators/src/index.ts
+++ packages/playwright-decorators/src/index.ts
@@ -4,5 +4,15 @@ export { test } from './decorators/test.decorator';
 export { beforeAll, afterAll, beforeEach, afterEach } from './decorators/hooks.decorator';
 export { tag, slow, skip, only, fail, fixme } from './decorators/utility.decorator';
 
+// Export API decorators
+export { ApiEndpoint } from './decorators/api-endpoint.decorator';
+export { PathParams } from './decorators/path-params.decorator';
+export { QueryParams } from './decorators/query-params.decorator';
+export { Headers } from './decorators/headers.decorator';
+export { RequestBody } from './decorators/request-body.decorator';
+export { ExpectStatus } from './decorators/expect-status.decorator';
+export { ExpectBody } from './decorators/expect-body.decorator';
+export { ExpectSchema } from './decorators/expect-schema.decorator';
+
 // Re-export types from core
 export * from '@verbaltest/playwright-core';
